6ff939a6d7ec5a82b11380baf6a1d9db9760a55a
zahid hasan <zahidhasan0562@gmail.com>
2026-02-16 21:44:03 +0600

    complete load catedgory product and display all

--- js/product.js
+++ js/product.js
@@ -4,3 +4,48 @@
 //  Example: https://fakestoreapi.com/products/category/jewelery
 //  Get 🛍️ Single Product Detail https://fakestoreapi.com/products/${id} 
 // Example: https://fakestoreapi.com/products/1
+
+
+
+const trendingProducts=async()=>{
+
+        const category = await fetch('https://fakestoreapi.com/products/category/jewelery')
+        const data =await category.json()
+         displayTrending(data)
+     
+
+}
+ const displayTrending=(trends)=>{
+    const products=trends;
+    const trendContainer=document.getElementById('trendin-container')
+    products.forEach(product=>{
+  const itemdiv=document.createElement('div');
+
+        itemdiv.innerHTML=`<div class="card bg-base-100 p-2 h-auto w-96 shadow-sm">
+                                <figure class="bg-orange-50">
+                                    <img class="h-56 w-56 p-2"
+                                    src="${product.image}"
+                                    alt="Shoes" />
+                                </figure>
+                                <div class="card-body h-60">
+                                    <div class="flex justify-between ">
+                                        <span class="bg-purple-100 text-purple-500 border py-1 px-2 rounded-full"> ${product.category} </span>
+                                        <span><i class="fa-solid fa-star" style="color: rgba(255, 212, 59, 1);"></i> ${product.rating.rate} (${product.rating.count}) </span>
+                                    </div>
+                                    <h2 class="card-title">${product.title}</h2>
+                                    <p class="text-lg font-bold">$ ${product.price}</p>
+                                    <div class="card-actions justify-between">
+                                        <button class="btn btn-outline btn-primary" onclick="my_modal_5.showModal(); productDetails('${product.id}')"> <i class="fa-solid fa-eye"></i>  Details</button>
+                                        
+                                        <button class="btn btn-primary" onclick="productDetails('${product.id}"> <i class="fa-solid fa-cart-shopping text-white"></i> Add</button>
+                                    </div>
+                                </div>
+                                </div>`
+
+                                trendContainer.append(itemdiv)
+
+    })
+   
+ }
+
+ trendingProducts();
--- js/script.js
+++ js/script.js
@@ -10,9 +10,7 @@ item.addEventListener('click',()=>{
 
     navItems.forEach(navitem=>navitem.classList.remove('active'))
 
-
     item.classList.add('active')
-    // console.log(item)
 
     if(item.innerText=='Home'){
         mainContentArea.innerHTML=homeContent;
@@ -25,11 +23,13 @@ item.addEventListener('click',()=>{
         const spinner=`<span class="loading loading-spinner loading-xl"></span>`
         mainContentArea.innerHTML=spinner;
 
-        mainContentArea.innerHTML=`<div>
-            <div id='category' class='w-1/3 mx-auto flex justify-center mt-5 gap-2'></div>
+        mainContentArea.innerHTML=`<div class="my-6">
+            <div id='category-container' class='w-1/3 mx-auto flex justify-center mt-5 gap-2' > </div>
             <div id='product-container' class="grid grid-cols-1 lg:grid-cols-4 md:grid-cols-2 gap-y-4 gap-x-3 mt-5 lg:px-40 sm:px-2 "></div>
         </div>`;
-        productItem()
+        productCategory();
+         productItem();
+       
     }
     else if(item.innerText=='About'){
         mainContentArea.innerHTML=''
@@ -38,71 +38,86 @@ item.addEventListener('click',()=>{
    else if(item.innerText=='Contact'){
         mainContentArea.innerHTML=''
     }
+});
+
+});
 
 
-})
+const productCategory=async()=>{
 
-
-})
-
-const productCategory=()=>{
-
-     const url=('https://fakestoreapi.com/products/categories')
-
-        fetch(url)
-        .then(res=>res.json())
-        .then(data=>displayProductCategory(data))
+        const category = await fetch('https://fakestoreapi.com/products/categories')
+        const data =await category.json()
+         displayProductCategory(data)
+     
 
 }
 
-const displayProductCategory=(categorys)=>{
 
-    const productcategory=document.getElementById('category')
+const displayProductCategory = (categorys) => {
+    
+    const productcategory = document.getElementById('category-container');
 
-          categorys.map(category => {
-            console.log(category)
-        const productContainer=document.getElementById('product-container')
+    const allbtn = document.createElement('button');
+    allbtn.className = 'btn btn-outline btn-primary activeP';
+    allbtn.textContent = 'All';
+    allbtn.addEventListener('click', () => {
+         productcategory.querySelectorAll('button').forEach(btn => {
+            btn.classList.remove('activeP');
+        });
 
-        const categoryheader=document.createElement('div');
+        allbtn.classList.add('activeP');
 
-        categoryheader.innerHTML=` <button id='' class="btn btn-outline btn-primary">   ${category} </button>`
-
-         productcategory.append(categoryheader)
-
+        productItem('all')
         
     });
+    productcategory.append(allbtn);
 
-}
+    categorys.forEach(category => {
+        const categoryBtn = document.createElement('button');
+        categoryBtn.className = 'btn btn-outline btn-primary';
+        categoryBtn.textContent = category;
+        categoryBtn.addEventListener('click', () => {
+              productcategory.querySelectorAll('button').forEach(btn => {
+            btn.classList.remove('activeP');
+        });
+
+        categoryBtn.classList.add('activeP');
+            productItem(category)});
+        productcategory.append(categoryBtn);
+    });
+
+   
+};
 
 
 
 
+const productItem=async(category='all')=>{
 
-const productItem=()=>{
+    if(category=="all"){
+         const url=await fetch('https://fakestoreapi.com/products')
+       const  data=await url.json()
+       displayProduct(data)
+    }
 
-        const url=('https://fakestoreapi.com/products')
+    else{
+         const url=await fetch(`https://fakestoreapi.com/products/category/${category}`)
+        const data=await url.json()
+       displayProduct(data)
+    }
+     
 
-        fetch(url)
-        .then(res=>res.json())
-        .then(data=>displayProduct(data))
+       
 
-}
-
-
-
+};
 
 const displayProduct=(data)=>{
 
-    
-
-
+    const productContainer=document.getElementById('product-container')
+     productContainer.innerHTML=''
     const products=data;
 
-     productCategory()
-
     products.forEach(product => {
-
-        const productContainer=document.getElementById('product-container')
 
         const itemdiv=document.createElement('div');
 
@@ -114,26 +129,61 @@ const displayProduct=(data)=>{
                                 </figure>
                                 <div class="card-body h-60">
                                     <div class="flex justify-between ">
-                                        <spnan class="bg-purple-400 text-white border p-1 rounded-full"> ${product.category} </spnan>
+                                        <span class="bg-purple-100 text-purple-500 border py-1 px-2 rounded-full"> ${product.category} </span>
                                         <span><i class="fa-solid fa-star" style="color: rgba(255, 212, 59, 1);"></i> ${product.rating.rate} (${product.rating.count}) </span>
                                     </div>
                                     <h2 class="card-title">${product.title}</h2>
-                                    <p>$ ${product.price}</p>
+                                    <p class="text-lg font-bold">$ ${product.price}</p>
                                     <div class="card-actions justify-between">
-                                        <button class="btn btn-outline btn-primary"> <i class="fa-solid fa-eye"></i>  Detailse </button>
-                                        <button class="btn btn-primary">Buy Now</button>
+                                        <button class="btn btn-outline btn-primary" onclick="my_modal_5.showModal(); productDetails('${product.id}')"> <i class="fa-solid fa-eye"></i>  Details</button>
+                                        
+                                        <button class="btn btn-primary" onclick="productDetails('${product.id}"> <i class="fa-solid fa-cart-shopping text-white"></i> Add</button>
                                     </div>
                                 </div>
                                 </div>`
          productContainer.append(itemdiv)
+// <button class="btn" onclick="my_modal_5.showModal()">open modal</button> 
 
         
     });
-
-
-
-
     
+};
 
+
+const productDetails=async(id)=>{
+       const url=await fetch(`https://fakestoreapi.com/products/${id}`);
+        const data=await url.json();
+       displayDetails(data)
+};
+
+const displayDetails=(product)=>{
     
+            const detaisContainer=document.getElementById('modal-content');
+            const itemdiv=document.createElement('div');
+            
+             itemdiv.innerHTML=`<div class="card bg-base-100 w-auto shadow-sm p-4">
+                                   
+                                    <div class="card-body h-60">
+                                        <div class="flex justify-between ">
+                                            <span class="bg-purple-400 text-white border p-1 rounded-full"> ${product.category} </span>
+                                            <span><i class="fa-solid fa-star" style="color: rgba(255, 212, 59, 1);"></i> ${product.rating.rate} (${product.rating.count}) </span>
+                                        </div>
+                                        <h2 class="card-title">${product.title}</h2>
+                                        <p class="text-sm opacity-65">${product.description}</p>
+                                        <p class="text-lg font-bold">$ ${product.price}</p>
+                                        
+                                    </div>
+                                </div>`
+         detaisContainer.append(itemdiv)
+
+
+
+
 }
+
+
+
+
+productDetails()
+;
+
